59a91a9e486c15e1654ca77a884326e58d395ee8
lxc <lemon_guess@163.com>
2023-08-03 19:45:30 +0800

    上传模块修改

--- demo/upload.py
+++ demo/upload.py
@@ -22,8 +22,8 @@ def precreate():
     with openapi_client.ApiClient() as api_client:
         # Create an instance of the API class
         api_instance = fileupload_api.FileuploadApi(api_client)
-        access_token = "121.9e0c308cf787c62576144c588201b4ae.YGVefo2fL-cKOtmLHwxyn4ZCime2I2LfGXE-2HQ.Gbci_Q"  # str |
-        path = "/apps/数据库备份/a.txt"  # str | 对于一般的第三方软件应用，路径以 "/apps/your-app-name/" 开头。对于小度等硬件应用，路径一般 "/来自：小度设备/" 开头。对于定制化配置的硬件应用，根据配置情况进行填写。
+        access_token = "121.36d1135ce89315f9d11e192c62010ab9.YmTOnTe9UekS6FinDzzggwGjcXn_w8Rj58MYsGQ.4DrvQg"  # str |
+        path = "/apps/数据备份/uploadtestdata"  # str | 对于一般的第三方软件应用，路径以 "/apps/your-app-name/" 开头。对于小度等硬件应用，路径一般 "/来自：小度设备/" 开头。对于定制化配置的硬件应用，根据配置情况进行填写。
         isdir = 0  # int | isdir
         size = 271  # int | size
         autoinit = 1  # int | autoinit
@@ -48,13 +48,13 @@ def upload():
     with openapi_client.ApiClient() as api_client:
         # Create an instance of the API class
         api_instance = fileupload_api.FileuploadApi(api_client)
-        access_token = "123.56c5d1f8eedf1f9404c547282c5dbcf4.YmmjpAlsjUFbPly3mJizVYqdfGDLsBaY5pyg3qL.a9IIIQ"  # str |
+        access_token = "121.36d1135ce89315f9d11e192c62010ab9.YmTOnTe9UekS6FinDzzggwGjcXn_w8Rj58MYsGQ.4DrvQg"  # str |
         partseq = "0"  # str |
-        path = "/apps/hhhkoo/a.txt"  # str |
+        path = "/apps/数据备份/a.txt"  # str |
         uploadid = "N1-MTA2LjEzLjc2LjI0MDoxNjU0NTAwMDE0OjE3NDEzNzMyMTUxNTY1MTA2MQ=="  # str |
         type = "tmpfile"  # str |
         try:
-            file = open('./uploadtestdata/a.txt', 'rb') # file_type | 要进行传送的本地文件分片
+            file = open('./uploadtestdata', 'rb') # file_type | 要进行传送的本地文件分片
         except Exception as e:
             print("Exception when open file: %s\n" % e)
             exit(-1)
@@ -77,10 +77,10 @@ def create():
     with openapi_client.ApiClient() as api_client:
         # Create an instance of the API class
         api_instance = fileupload_api.FileuploadApi(api_client)
-        access_token = "123.56c5d1f8eedf1f9404c547282c5dbcf4.YmmjpAlsjUFbPly3mJizVYqdfGDLsBaY5pyg3qL.a9IIIQ"  # str |
-        path = "/apps/hhhkoo/a.txt"  # str | 与precreate的path值保持一致
-        isdir = 0  # int | isdir
-        size = 271 # int | 与precreate的size值保持一致
+        access_token = "121.36d1135ce89315f9d11e192c62010ab9.YmTOnTe9UekS6FinDzzggwGjcXn_w8Rj58MYsGQ.4DrvQg"  # str |
+        path = "/apps/数据备份/uploadtestdata"  # str | 与precreate的path值保持一致
+        isdir = 1  # int | isdir
+        size = 0 # int | 与precreate的size值保持一致
         uploadid = "N1-MTA2LjEzLjc2LjI0MDoxNjU0NTAwMDE0OjE3NDEzNzMyMTUxNTY1MTA2MQ=="  # str | precreate返回的uploadid
         block_list = '["d05f84cf5340d1ef0c5f6d6eb8ce13b8"]'  # str | 与precreate的block_list值保持一致
         rtype = 3  # int | rtype (optional)
